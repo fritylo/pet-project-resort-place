15de7c9c2d0e2770580fde29edbefaf806c111e1
fritylo <to.fritylo@gmail.com>
2023-04-04 20:37:44 +0300

    feat(init)

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resort Places</title>
+</head>
+<body>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi consectetur aut repellat inventore, rem dolor molestiae eveniet nihil totam necessitatibus amet ad laudantium unde facere optio voluptatibus consequatur dolorem, saepe omnis nemo sequi. Et voluptatem nihil vitae atque sit, excepturi dolorum culpa debitis quod fugit doloribus voluptates fuga, provident, corrupti itaque harum! Aliquam nam eaque iure commodi fuga voluptas ullam autem praesentium vero, facere quo aperiam. Tempora, voluptatem, quas aut nostrum unde eveniet nemo repellendus fugiat autem, iure laudantium quam impedit animi voluptates! Magnam deserunt fugiat quibusdam nemo, minima illum autem obcaecati iste, mollitia porro delectus, quos possimus quod recusandae!
+</body>
+</html>
